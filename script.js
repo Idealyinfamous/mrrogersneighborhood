de50891852a9edbf3d354103432fc5cda021dd09
Jessica Perry <l33thedgieperry@gmail.com>
2023-06-10 23:38:17 -0700

    script test success for User input and list values
    This change retrieves user input and lists out value count
    from users input logs to console.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-//Takes a number from the input quantity
+//1)Takes a number from the input quantity
 let userInput = document.getElementById("quantity").value;
-console.log(userInput);
+//console.log(userInput);
 //SUCCESS!!! tested in console! retrieves inputed number
 
+//2)return a list of values and start from 0 to users input number
+for ( i = 0; i <= userInput; i++) {
+    console.log(i)
+   }
+//success!!!! tested: lists out value count from users input.
